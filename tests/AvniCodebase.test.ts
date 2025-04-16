eeb6bef8304e844585cf558141075afaf10ea123
Vivek Singh <petmongrels@gmail.com>
2025-04-16 16:04:39 +0530

    #1716 - fixed test

--- tests/AvniCodebase.test.ts
+++ tests/AvniCodebase.test.ts
@@ -11,8 +11,7 @@ it('should get ancestor branch', function () {
 
 it('should get ancestor branch', function () {
     const project = {name: "rules-config", "main-branch": "master"};
-    expect(AvniCodebase.getAncestorBranch("12.0", project)).toBe("master");
-    expect(AvniCodebase.getAncestorBranch("12.1", project)).toBe("12.0");
-    expect(AvniCodebase.getAncestorBranch("12.2", project)).toBe("12.1");
+    const releases = AvniCodebase.getReleases();
+    expect(AvniCodebase.getAncestorBranch(releases[1], project)).toBe(releases[0]);
     expect(AvniCodebase.getAncestorBranch("master", project)).toBe(_.last(AvniCodebase.getReleases()));
 });
